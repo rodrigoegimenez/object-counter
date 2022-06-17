656b15387c3c6205b6ba26b78528c9bee505a3ac
Rodrigo Gimenez <rodrigog83@gmail.com>
2022-06-17 08:21:32 +0200

    Implement prediction endpoint for cli

--- counter/entrypoints/main.py
+++ counter/entrypoints/main.py
@@ -1,11 +1,31 @@
 import sys
+import click
 
 from counter import config
 
-if __name__ == '__main__':
-    img_path = sys.argv[1]
-    threshold = float(sys.argv[2])
-    with open(img_path, 'rb') as img:
-        predictions = config.get_count_action().execute(img, threshold)
 
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('-i', "--img-path", required=True, help="The path to the image to be processed")
+@click.option('-t', "--threshold", type=float, default=0.9, help="The lower limit for the object detection")
+def count(img_path: str, threshold: float):
+    with open(img_path, "rb") as img:
+        predictions = config.get_count_action().execute(img, threshold)
         print(predictions)
+
+
+@cli.command()
+@click.option('-i', "--img-path", required=True, help="The path to the image to be processed")
+@click.option('-t', "--threshold", type=float, default=0.9, help="The lower limit for the object detection")
+def predict(img_path: str, threshold: float):
+    with open(img_path, "rb") as img:
+        predictions = config.get_prediction_action().execute(img, threshold)
+        print(predictions)
+
+
+if __name__ == "__main__":
+    cli()
