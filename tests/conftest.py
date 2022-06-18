eacac2e644ba3c69991cadb93309fb948ecaf550
Rodrigo Gimenez <rodrigog83@gmail.com>
2022-06-18 09:16:34 +0200

    Add count objects endpoint using fake detector functional test

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+from counter.entrypoints.webapp import app
+
+@pytest.fixture(scope="module")
+def test_client():
+    with app.test_client() as test_client:
+        with app.app_context():
+            yield test_client
---
+++ tests/integration/test_endpoints.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+
+def test_home_page_provides_swagger_ui(test_client):
+    response = test_client.get("/")
+    assert "Swagger" in response.text
+
+
+resources = Path(__file__).parent.parent.parent / "resources"
+
+
+def test_counting_using_fake_detector(test_client):
+    response = test_client.post("/object-count", data={"file": (resources / "images" / "boy.jpg").open("rb"), })
+    assert response.status_code == 200
