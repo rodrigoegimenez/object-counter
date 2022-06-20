fc28d12e9e33617604c69b6511db66f03da5ecf4
Rodrigo Gimenez <rodrigog83@gmail.com>
2022-06-20 22:36:52 +0200

    Fix download model script

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-wget https://storage.googleapis.com/intel-optimized-tensorflow/models/v1_8/rfcn_resnet101_fp32_coco_pretrained_model.tar.gz
+wget \
+  --no-verbose \
+  --show-progress \
+  --progress=dot:giga \
+  https://storage.googleapis.com/intel-optimized-tensorflow/models/v1_8/rfcn_resnet101_fp32_coco_pretrained_model.tar.gz
+
 tar -xzvf rfcn_resnet101_fp32_coco_pretrained_model.tar.gz -C tmp
 rm rfcn_resnet101_fp32_coco_pretrained_model.tar.gz
 chmod -R 777 tmp/rfcn_resnet101_coco_2018_01_28
-mkdir -p tmp/model/1
-mv tmp/rfcn_resnet101_coco_2018_01_28/saved_model/saved_model.pb tmp/model/1
+mkdir -p tmp/model/rfcn/1
+mv tmp/rfcn_resnet101_coco_2018_01_28/saved_model/saved_model.pb tmp/model/rfcn/1
 rm -rf tmp/rfcn_resnet101_coco_2018_01_28
 echo "Delete this file to force model download." >download_model
 
